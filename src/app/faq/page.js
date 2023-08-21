31edc6d4d9db15e4abc5ce6b3fdebc65b3a8d783
Jossif Elefteriadis <jossif_@hotmail.com>
2023-08-21 21:35:45 +0200

    display footer in pages

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -1,11 +1,13 @@
 import Header from "../components/header";
 import FAQComp from "../components/faq";
+import Footer from "../components/footer";
 
 export default function FAQ() {
   return (
     <section>
       <Header />
       <FAQComp />
+      <Footer />
     </section>
   );
 }
--- src/app/how-it-works/page.js
+++ src/app/how-it-works/page.js
@@ -1,11 +1,13 @@
 import Header from "../components/header";
 import HowItWorksComp from "../components/howItWorks";
+import Footer from "../components/footer";
 
 export default function HowItWorks() {
   return (
     <section>
       <Header />
       <HowItWorksComp />
+      <Footer />
     </section>
   );
 }
