01c4107e88f5d0d1a089bfefee09dffa9f021d44
Jossif Elefteriadis <jossif_@hotmail.com>
2023-08-22 16:36:39 +0200

    display components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 import Header from "./components/header";
+import Hero from "./components/hero";
+import HomeComp from "./components/home";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-[#EAE0CC]">
+    <main className="flex min-h-screen flex-col items-center bg-white">
       <Header />
+      <Hero />
+      <HomeComp />
+      <Footer />
     </main>
   );
 }
