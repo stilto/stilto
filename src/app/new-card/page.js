ad8476ef194812752042a389595359cca0c42cbd
Jossif Elefteriadis <jossif_@hotmail.com>
2023-08-22 16:55:34 +0200

    Display components

---
+++ src/app/new-card/page.js
@@ -0,0 +1,13 @@
+import Header from "../components/header";
+import NewCardComp from "../components/newCard";
+import Footer from "../components/footer";
+
+export default function NewCard() {
+  return (
+    <section>
+      <Header />
+      <NewCardComp />
+      <Footer />
+    </section>
+  );
+}
