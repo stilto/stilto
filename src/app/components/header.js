b647126cc7cae76152ac8096d5418b132528eed3
Jossif Elefteriadis <jossif_@hotmail.com>
2023-08-22 10:40:53 +0200

    header mobile responsive

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -7,7 +7,7 @@ const parisienne = Parisienne({ weight: "400", subsets: ["latin"] });
 
 export default function Header() {
   return (
-    <section className="w-screen h-20 flex justify-between items-center px-10 bg-white text-gray-900 text-xl border-b border-gray-300 tracking-wider sticky top-0">
+    <section className="w-screen h-20 flex justify-between items-center px-10 bg-white text-gray-900 text-xl border-b border-gray-300 tracking-wider sticky top-0 z-50">
       <section className="w-1/2 h-10 flex items-center">
         <h1
           className={`${parisienne.className} h-14 flex items-center text-5xl font-semibold mr-2 px-2`}
@@ -15,7 +15,7 @@ export default function Header() {
           <Link href="/">stilto</Link>
         </h1>
       </section>
-      <section className="w-1/6 flex justify-evenly">
+      <section className="w-2/3 md:w-1/3 lg:w-1/4 xl:w-1/6 flex justify-evenly">
         <Link href="/how-it-works">How it works</Link>
         <Link href="/faq">FAQ</Link>
       </section>
