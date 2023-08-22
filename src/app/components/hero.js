e82b32a98a0c2d5a90c26296572a9077a834bd8a
Jossif Elefteriadis <jossif_@hotmail.com>
2023-08-22 10:41:48 +0200

    Create hero comp and home page comp

---
+++ src/app/components/hero.js
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { Ubuntu } from "next/font/google";
+
+const ubuntu = Ubuntu({ weight: "400", subsets: ["greek"] });
+
+import HBCard from "../assets/hb-startPage.png";
+import NYCard from "../assets/ny-startPage.png";
+
+export default function Hero() {
+  return (
+    <section className="w-full flex justify-center bg-[#EAE0CC] py-16 overflow-hidden">
+      <section className="w-full flex flex-col md:flex-row items-center">
+        <section className="w-full md:w-1/2 flex justify-center md:justify-end">
+          <Image
+            src={NYCard}
+            alt="Happy New Year Card"
+            width={300}
+            height={300}
+            className="md:w-1/2 xl:w-auto origin-left rotate-12 z-20 drop-shadow-2xl"
+          />
+          <Image
+            src={HBCard}
+            alt="Happy Birthday Card"
+            width={300}
+            height={300}
+            className="md:w-1/2 xl:w-auto md:mr-10 origin-bottom-left -rotate-12 drop-shadow-2xl"
+          />
+        </section>
+        <section className="w-3/4 md:w-1/2 flex flex-col justify-center mt-20 md:mt-0">
+          <p
+            className={`${ubuntu.className} w-full text-gray-900 text-2xl md:text-2xl lg:text-3xl font-semibold`}
+          >
+            The easiest way to send crypto
+            <br /> along with a greeting card
+          </p>
+          <p className="md:w-2/3 text-gray-900 mt-4">
+            Create a greeting card, add your personal message, and send the
+            desired crypto amount to a friend that claims via a link.
+          </p>
+          <button className="w-44 h-12 bg-[#877b9a] hover:bg-[#8f82a5] text-lg mt-6 rounded-xl">
+            Start Creating
+          </button>
+        </section>
+      </section>
+    </section>
+  );
+}
---
+++ src/app/components/home.js
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import { Ubuntu } from "next/font/google";
+
+const ubuntu = Ubuntu({ weight: "400", subsets: ["greek"] });
+import GC from "../assets/gc.png";
+import Wallet from "../assets/wallet.png";
+import Send from "../assets/send.png";
+
+export default function HomeComp() {
+  return (
+    <section className="w-full flex flex-col items-center text-gray-900 mt-10">
+      <h1 className={`${ubuntu.className} text-3xl font-semibold`}>
+        HOW IT WORKS
+      </h1>
+      <section className="w-3/4 flex flex-col md:flex-row md:justify-between mt-8">
+        <section className="flex flex-col items-center mt-10 px-2">
+          <Image src={GC} alt="greeting card" width={100} height={100} />
+          <p className="text-xl font-semibold text-center mt-2">Pick a card</p>
+          <p className="text-lg mt-2 text-center">
+            Chose one of our beautiful cards
+          </p>
+        </section>
+        <section className="flex flex-col items-center mt-10 px-10">
+          <Image src={Wallet} alt="crypto wallet" width={100} height={100} />
+          <p className="text-xl font-semibold text-center mt-2">
+            Add crypto & message
+          </p>
+          <p className="text-lg mt-2 text-center">
+            Add the crypto amount you want to give, along with a thoughtful or
+            funny message
+          </p>
+        </section>
+        <section className="flex flex-col items-center mt-10 px-2">
+          <Image src={Send} alt="send crypto" width={100} height={100} />
+          <p className="text-xl font-semibold text-center mt-2">Send</p>
+          <p className="text-lg mt-2 text-center">
+            Send the unique link to your friend
+          </p>
+        </section>
+      </section>
+      <button className="w-64 h-16 bg-[#877b9a] hover:bg-[#8f82a5] text-white text-xl my-20 rounded-xl">
+        Start Creating
+      </button>
+      <section className="w-full flex flex-col items-center bg-[#EAE0CC] pt-8 pb-20">
+        <h1 className={`${ubuntu.className} text-3xl font-semibold`}>
+          ADD GIFS
+        </h1>
+        <p className="text-lg mt-2">
+          Want to send a GIF with crypto? No worries!
+        </p>
+        <section className="w-full flex flex-col lg:flex-row items-center mt-12">
+          <section className="w-full lg:w-2/3 flex justify-center">
+            <video
+              autoPlay
+              muted
+              loop
+              className="w-48 lg:w-64 h-48 lg:h-64 rounded-lg"
+            >
+              <source src="/hb.mp4" type="video/mp4" />
+            </video>
+            <video
+              autoPlay
+              muted
+              loop
+              className="w-48 lg:w-56 h-48 lg:h-56 mt-36 -ml-32 rounded-lg"
+            >
+              <source src="/gm.mp4" type="video/mp4" />
+            </video>
+            <video
+              autoPlay
+              muted
+              loop
+              className="w-48 lg:w-60 h-48 lg:h-60 mt-10 -ml-12 rounded-lg"
+            >
+              <source src="/to.mp4" type="video/mp4" />
+            </video>
+          </section>
+          <section className="w-full lg:w-1/3 text-center text-lg mt-8 lg:mt-0 px-10 sm:px-20 md:px-40 lg:px-0 lg:pr-10">
+            <p>
+              Want to send a GIF instead of a card? That&apos;s not an issue!
+              Search through our library of GIFs and chose the best one for the
+              occasion
+            </p>
+          </section>
+        </section>
+      </section>
+      <button className="w-64 h-16 bg-[#877b9a] hover:bg-[#8f82a5] text-white text-xl my-20 rounded-xl">
+        Start Creating
+      </button>
+    </section>
+  );
+}
