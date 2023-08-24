a3a765f6112ffea43f85bebadeed1042a7d73c77
Jossif Elefteriadis <jossif_@hotmail.com>
2023-08-24 10:13:16 +0200

    refactor

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -32,11 +32,12 @@ export default function Hero() {
             className={`${ubuntu.className} w-full text-gray-900 text-2xl md:text-2xl lg:text-3xl font-semibold`}
           >
             The easiest way to send crypto
-            <br /> along with a greeting card
+            <br /> along with a greeting card or a gif
           </p>
           <p className="md:w-2/3 text-gray-900 mt-4">
-            Create a greeting card, add your personal message, and send the
-            desired crypto amount to a friend that claims via a link.
+            Create a greeting card or chose a gif, add your personal message,
+            and send the desired crypto amount to a friend that claims via a
+            link.
           </p>
           <Link href="/new-card">
             <button className="w-44 h-12 bg-[#877b9a] hover:bg-[#8f82a5] text-lg mt-6 rounded-xl">
