d3d2276c2259cf6e0ebd4287ea28672773abcaac
Jossif Elefteriadis <jossif_@hotmail.com>
2023-08-24 10:38:06 +0200

    Add connectWallet comp

---
+++ src/app/components/connectWallet.js
@@ -0,0 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function ConnectWallet() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [currentAccount]);
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  return (
+    <section className="mt-10">
+      {!currentAccount && (
+        <button
+          onClick={connectWallet}
+          className="w-60 h-14 bg-[#877b9a] hover:bg-[#8f82a5] text-white text-lg mt-6 rounded-xl uppercase"
+        >
+          Connect Wallet
+        </button>
+      )}
+    </section>
+  );
+}
