b176687f18fd47aee5881b4e172a0efa34d85811
Jossif Elefteriadis <jossif_@hotmail.com>
2023-09-09 17:31:41 +0200

    refactor text and styling

--- frontend/app/components/hero.js
+++ frontend/app/components/hero.js
@@ -9,7 +9,7 @@ import NYCard from "../assets/ny-startPage.png";
 
 export default function Hero() {
   return (
-    <section className="w-full flex justify-center bg-[#EAE0CC] py-16 overflow-hidden">
+    <section className="w-full flex justify-center bg-[#EAE0CC] py-24 overflow-hidden">
       <section className="w-full flex flex-col md:flex-row items-center">
         <section className="w-full md:w-1/2 flex justify-center md:justify-end">
           <Image
@@ -27,20 +27,20 @@ export default function Hero() {
             className="md:w-1/2 xl:w-auto md:mr-10 origin-bottom-left -rotate-12 drop-shadow-2xl"
           />
         </section>
-        <section className="w-3/4 md:w-1/2 flex flex-col justify-center mt-20 md:mt-0">
+        <section className="w-3/4 md:w-1/2 h-full flex flex-col justify-center mt-20 md:mt-0">
           <p
-            className={`${ubuntu.className} w-full text-gray-900 text-2xl md:text-2xl lg:text-3xl font-semibold`}
+            className={`${ubuntu.className} w-full text-gray-900 text-2xl md:text-2xl lg:text-4xl font-semibold`}
           >
             The easiest way to send crypto
-            <br /> along with a greeting card or a gif
+            <br /> along with a digital greeting card or a gif
           </p>
-          <p className="md:w-2/3 text-gray-900 mt-4">
-            Create a greeting card or chose a gif, add your personal message,
-            and send the desired crypto amount to a friend that claims via a
-            link.
+          <p className="md:w-2/3 text-gray-900 mt-10 md:text-lg">
+            Create digital greeting cards or chose a gif, add your personal
+            message, and send the desired crypto amount to a friend that claims
+            via a link.
           </p>
-          <Link href="/add-card">
-            <button className="w-44 h-12 bg-[#877b9a] hover:bg-[#8f82a5] text-lg mt-6 rounded-xl">
+          <Link href="/add-card" className="mt-6">
+            <button className="w-44 h-12 bg-[#877b9a] hover:bg-[#8f82a5] text-lg rounded-xl">
               Start Sending
             </button>
           </Link>
