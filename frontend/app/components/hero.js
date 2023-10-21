8e0cdaac9fdd7d44d8de223b1172ae097496417f
Jossif Elefteriadis <jossif_@hotmail.com>
2023-10-21 22:45:37 +0200

    redo hero section

--- frontend/app/components/hero.js
+++ frontend/app/components/hero.js
@@ -1,27 +1,27 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Ubuntu } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const ubuntu = Ubuntu({ weight: "400", subsets: ["greek"] });
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 import HBCard from "../assets/hb-startPage.png";
 import NYCard from "../assets/ny-startPage.png";
 
 export default function Hero() {
   return (
-    <section className="w-full flex justify-center bg-[#EAE0CC] py-24 overflow-hidden">
+    <section className="w-full flex justify-center bg-[#e0f7fa] py-24 overflow-hidden">
       <section className="w-full flex flex-col md:flex-row items-center">
         <section className="w-full md:w-1/2 flex justify-center md:justify-end">
           <Image
             src={NYCard}
-            alt="Happy New Year Card"
+            alt="Happy New Year Card with Crypto Coin"
             width={300}
             height={300}
             className="md:w-1/2 xl:w-auto origin-left rotate-12 z-20 drop-shadow-2xl"
           />
           <Image
             src={HBCard}
-            alt="Happy Birthday Card"
+            alt="Happy Birthday Card with Crypto Coin"
             width={300}
             height={300}
             className="md:w-1/2 xl:w-auto md:mr-10 origin-bottom-left -rotate-12 drop-shadow-2xl"
@@ -29,18 +29,18 @@ export default function Hero() {
         </section>
         <section className="w-3/4 md:w-1/2 h-full flex flex-col justify-center mt-20 md:mt-0">
           <p
-            className={`${ubuntu.className} w-full text-gray-900 text-2xl md:text-2xl lg:text-4xl font-semibold`}
+            className={`${poppins.className} w-full text-[#004d40] text-2xl md:text-2xl lg:text-4xl font-semibold`}
           >
-            The easiest way to send crypto
-            <br /> along with a digital greeting card or a gif
+            Send Crypto with a Personal Touch
+            <br /> via Digital Cards or Gifs
           </p>
-          <p className="md:w-2/3 text-gray-900 mt-10 md:text-lg">
-            Create digital greeting cards or choose a gif, add your personal
-            message, and send the desired crypto amount to a friend that claims
-            via a link.
+          <p className="md:w-2/3 text-[#004d40] mt-10 md:text-lg">
+            Craft digital greeting cards or pick a fun gif, add your heartfelt
+            message, and send crypto to a friend who can claim it through a
+            link.
           </p>
           <Link href="/add-card" className="mt-6">
-            <button className="w-44 h-12 bg-[#877b9a] hover:bg-[#8f82a5] text-lg rounded-xl">
+            <button className="w-44 h-12 bg-[#1de9b6] hover:bg-[#00bfa5] text-lg rounded-xl">
               Start Sending
             </button>
           </Link>
