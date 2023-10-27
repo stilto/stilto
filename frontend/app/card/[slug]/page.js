7625aea26b4bac8333baaecbb9a1e065dc65ad5d
Jossif Elefteriadis <jossif_@hotmail.com>
2023-10-27 13:21:09 +0200

    add footer

--- frontend/app/card/[slug]/page.js
+++ frontend/app/card/[slug]/page.js
@@ -1,10 +1,12 @@
 import ClaimButton from "../../components/claimButton";
+import Footer from "../../components/footer";
 
 export default async function Page({ params }) {
   return (
     <section>
       <p>page: {params.slug}</p>
       <ClaimButton />
+      <Footer />
     </section>
   );
 }
