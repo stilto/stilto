e8a4b7914c70f351c257f79c928e918857b84d66
Jossif Elefteriadis <jossif_@hotmail.com>
2023-10-28 18:48:42 +0200

    WIP fix issue

--- frontend/app/components/addCrypto.js
+++ frontend/app/components/addCrypto.js
@@ -147,7 +147,7 @@ export default function AddCryptoComp() {
             ) : (
               <section className="text-center break-all">
                 Share this claimable link:{" "}
-                {`http://www.stilto.com/card/claim?from=${currentAccount}&title=${title.replaceAll(
+                {`https://www.stilto.com/card/claim?from=${currentAccount}&title=${title.replaceAll(
                   " ",
                   "-"
                 )}&message=${message.replaceAll(" ", "-")}`}
