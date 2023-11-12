66b9efa0e88fdc5d21bf1b11b1ec6a1574e10c8f
Jossif Elefteriadis <jossif_@hotmail.com>
2023-11-12 21:01:11 +0100

    Add spinner, display giftId in claimURL

--- frontend/app/components/addCrypto.js
+++ frontend/app/components/addCrypto.js
@@ -17,6 +17,9 @@ export default function AddCryptoComp() {
   //   const [chainId, setChainId] = useState(null);
   const [amount, setAmount] = useState("");
   const [link, setLink] = useState("");
+  const [giftId, setGiftId] = useState("");
+  const [giftLinkReady, setGiftLinkReady] = useState(false);
+  const [loadingLink, setLoadingLink] = useState(false);
   const [linkStatus, setLinkStatus] = useState(null);
   //   const [isConnected, setIsConnected] = useState(false);
   const [claimTx, setClaimTx] = useState(null);
@@ -64,6 +67,7 @@ export default function AddCryptoComp() {
       "createLinkResponse here :)",
       createLinkResponse.createdLink.link[0]
     );
+    setLoadingLink(true);
     setLink(createLinkResponse.createdLink.link[0]);
     createClaimUrl(createLinkResponse.createdLink.link[0]);
   };
@@ -89,14 +93,21 @@ export default function AddCryptoComp() {
   };
 
   const createClaimUrl = async (link) => {
-    await axios.post("https://api.stilto.io/createclaimurl", {
-      sender: currentAccount,
-      gif: chosenGif,
-      card: chosenCard,
-      title,
-      message,
-      claimLink: link,
-    });
+    await axios
+      .post("https://api.stilto.io/createclaimurl", {
+        sender: currentAccount,
+        gif: chosenGif,
+        card: chosenCard,
+        title,
+        message,
+        amount,
+        claimLink: link,
+      })
+      .then((response) => {
+        setGiftId(response.data);
+        setGiftLinkReady(true);
+        setLoadingLink(false);
+      });
   };
 
   return (
@@ -118,7 +129,11 @@ export default function AddCryptoComp() {
       {currentAccount && (
         <section className="w-full h-screen flex flex-col lg:flex-row lg:justify-evenly items-center lg:items-start bg-[#e0f7fa] text-[#004d40] mt-6">
           <section className="w-full lg:w-2/5 h-60 lg:h-3/4 flex flex-col p-2 rounded-lg shadow-lg bg-white">
-            <section className="flex justify-between md:justify-evenly items-center mt-10 mb-4">
+            <section className="text-center text-lg mt-6">
+              Add amount and click &apos;Create link&apos; to get your claimable
+              link.
+            </section>
+            <section className="flex justify-between md:justify-evenly items-center mt-8 mb-4">
               <label htmlFor="amount" className="text-lg">
                 ETH amount to gift:
               </label>
@@ -139,18 +154,34 @@ export default function AddCryptoComp() {
                 </button>
               </section>
             </section>
-            {!link ? (
-              <section className="text-center">
-                Add amount and click &apos;Create link&apos; to get your
-                claimable link.
-              </section>
+            {!giftLinkReady ? (
+              loadingLink && (
+                <section className="text-center mt-4">
+                  <svg
+                    aria-hidden="true"
+                    className="inline w-8 h-8 text-gray-300 animate-spin dark:fill-[#00bfa5]"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                </section>
+              )
             ) : (
-              <section className="text-center break-all">
+              <section className="text-center break-all mt-4">
+                {/*TODO:
+                Add icon that copies to clipboard
+                */}
                 Share this claimable link:{" "}
-                {`https://stilto.io/card/claim?from=${currentAccount}&title=${title.replaceAll(
-                  " ",
-                  "-"
-                )}&message=${message.replaceAll(" ", "-")}`}
+                {`https://stilto.io/card/claim?id=${giftId}`}
               </section>
             )}
           </section>
@@ -178,6 +209,7 @@ export default function AddCryptoComp() {
               </h1>
             )}
             {message && <p className="w-full text-center mt-2">{message}</p>}
+            <p className="w-full text-center mt-2">Gift amount: {amount} ETH</p>
           </section>
         </section>
       )}
