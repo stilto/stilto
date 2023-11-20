8bf84825d35aab94119ae524edb98dd3c3cd9367
Jossif Elefteriadis <jossif_@hotmail.com>
2023-11-20 12:01:10 +0100

    add testimonial

--- frontend/app/components/home.js
+++ frontend/app/components/home.js
@@ -73,21 +73,8 @@ export default function HomeComp() {
       <section className="w-2/3 mt-8">
         <section className="flex items-center divide-x divide-black my-4">
           <section className="flex items-center space-x-4 text-black pr-8">
-            {/* <Image
-            className="w-10 h-10 rounded-full"
-            src={GC}
-            alt="Profile pic"
-          /> */}
             <section className="space-y-1 font-medium">
-              <p>
-                Jese Leos
-                {/* <time
-                  dateTime="2014-08-16 19:00"
-                  className="block text-sm text-gray-500 dark:text-gray-400"
-                >
-                  September 2023
-                </time> */}
-              </p>
+              <p>Tom</p>
             </section>
           </section>
           <section className="pl-8">
@@ -129,7 +116,7 @@ export default function HomeComp() {
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
               </svg>
               <svg
-                className="w-4 h-4 text-gray-300 dark:text-gray-500 mr-1"
+                className="w-4 h-4 text-yellow-300 mr-1"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -143,22 +130,16 @@ export default function HomeComp() {
             </section>
             <section className="text-sm text-gray-500 dark:text-gray-400">
               <p>
-                Reviewed in the United Kingdom on{" "}
-                <time dateTime="2017-03-03 19:00">September 3, 2023</time>
+                Reviewed in the UAE on{" "}
+                <time dateTime="2017-03-03 19:00">November 20, 2023</time>
               </p>
             </section>
           </section>
         </section>
         <p className="mb-2 text-gray-500 dark:text-gray-400">
-          This is my third Invicta Pro sectioner. They are just fantastic value
-          for money. This one arrived yesterday and the first thing I did was
-          set the time, popped on an identical strap from another Invicta and
-          went in the shower with it to test the waterproofing.... No problems.
-        </p>
-        <p className="mb-3 text-gray-500 dark:text-gray-400">
-          It is obviously not the same build quality as those very expensive
-          watches. But that is like comparing a Citroën to a Ferrari. This watch
-          was well under £100! An absolute bargain.
+          So far Stilto has been the easiest and most convenient way to send
+          crypto gifts. I'm amazed that so many gift cards are available and any
+          GIF can also be used to surprise my friends!
         </p>
       </section>
 
