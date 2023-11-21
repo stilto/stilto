9e6a5023847638e6811e621e35c6fb1c8bbb449a
Jossif Elefteriadis <jossif_@hotmail.com>
2023-11-21 08:34:03 +0100

    add web3modal and wagmi

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,15 +1,30 @@
 "use client";
 import { useState } from "react";
 import "./globals.css";
-import { Inter, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
+import { createConfig, WagmiConfig, mainnet, sepolia } from "wagmi";
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
 
 import Context from "./utils/context";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "stilto - send a gift card with crypto",
-// };
+const projectId = process.env.NEXT_PUBLIC_WEB3MODAL_PROJECT_ID;
+
+const metadata = {
+  name: "stilto",
+  description: "send a gift card with crypto",
+  url: "https://stilto.io",
+};
+
+const chains = [mainnet, sepolia];
+const wagmiConfig = createConfig({ chains, projectId, metadata });
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains,
+  projectId,
+});
 
 export default function RootLayout({ children }) {
   const [chosenGif, setChosenGif] = useState("");
@@ -32,7 +47,7 @@ export default function RootLayout({ children }) {
             setMessage,
           }}
         >
-          {children}
+          <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
         </Context.Provider>
       </body>
     </html>
