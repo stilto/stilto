15f73b0b3e58d904d28d4f3f0a32e366f53410c1
Jossif Elefteriadis <jossif_@hotmail.com>
2024-01-03 23:36:00 +0100

    Add get req for wallet nfts

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Moralis from "moralis";
 import cors from "cors";
 import dotenv from "dotenv";
 const app = express();
@@ -12,6 +13,8 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
+const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
+
 app.get("/getclaimurl", async (req, res) => {
   try {
     const { query } = req;
@@ -24,6 +27,23 @@ app.get("/getclaimurl", async (req, res) => {
     return res.status(200).json(allGifts);
   } catch (e) {
     console.log("error", e);
+    return res.status(400).json();
+  }
+});
+
+app.get("/getwalletnfts", async (req, res) => {
+  try {
+    const { query } = req;
+    const response = await Moralis.EvmApi.nft.getWalletNFTs({
+      chain: "0x1",
+      format: "decimal",
+      mediaItems: true,
+      address: query.address,
+    });
+
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(`Something went wrong ${e}`);
     return res.status(400).json();
   }
 });
@@ -60,6 +80,10 @@ app.post("/setgiftclaimed", async (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+Moralis.start({
+  apiKey: MORALIS_API_KEY,
+}).then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 });
